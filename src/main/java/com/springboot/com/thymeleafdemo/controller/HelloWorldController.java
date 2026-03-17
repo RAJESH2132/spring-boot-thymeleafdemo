7d2b207a99b632c2d41f8604a3a78457e8439a1c
RAJESH2132 <rajeshnagurla@gmail.com>
2026-03-17 22:51:03 +0530

    add data to mvc model without RequestParam annotation

--- src/main/java/com/springboot/com/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/springboot/com/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.springboot.com.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +16,23 @@ public class HelloWorldController {
     //need a controller method to process the HTML form
     @RequestMapping("/processForm")
     public String processForm(){
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String formVersionTwo(HttpServletRequest request, Model model){
+        //read req parameter from html form
+        String theName = request.getParameter("studentName");
+
+        //convert the data to all caps
+        theName = theName.toUpperCase();
+
+        //create message
+        String result = "Yo!" + theName;
+
+        //add message to model
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 
